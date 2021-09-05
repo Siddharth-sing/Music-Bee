84380debb171ea5254f5e27c546d7010c477fb51
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-09-05 21:53:15 +0530

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:music_bee/pages/Homepage.dart';
 
 void main() {
+
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
   runApp(MyApp());
 }
 
@@ -11,7 +17,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
       home: Homepage(),
     );
   }
--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:music_bee/model/radio.dart';
 import 'package:music_bee/utils/aiUtils.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -8,12 +11,66 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+
+  List<MyRadio> radios;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchRadios();
+  }
+
+  fetchRadios() async{
+    final radioJson = await rootBundle.loadString("asset/radio.json");
+    radios =  MyRadioList.fromJson(radioJson).radios;
+    print(radios);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: Container(color: aiColors.primaryolor1),
+    return Scaffold(
+      drawer: Drawer(),
+      body: Stack(
+        children:<Widget> [
+          VxAnimatedBox()
+            .size(context.screenWidth, context.screenHeight)
+            .withGradient(
+          LinearGradient(
+            colors: [
+              aiColors.primaryolor1,
+              aiColors.primaryolor2,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ).make(),
+          AppBar(
+            title: Text(
+                'AI Radio',
+              style: TextStyle(
+                fontFamily: GoogleFonts.poppins().fontFamily,
+                fontSize: 28,
+              ),
+            ).shimmer(
+              primaryColor: Vx.purple300,
+              secondaryColor: Colors.white,
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+          )
+          
+          VxSwiper.builder(
+              itemCount: radios.length,
+              aspectRatio: 1.0,
+              itemBuilder: (context, index){
+
+              }
+          ),
+          
+        ],
+      ),
     );
   }
 }
